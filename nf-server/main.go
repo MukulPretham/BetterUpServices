8a3754a4b3e1cc2d26a0a0a2b586120acabcff26
Mukul Pretham <mukulpretham@gmail.com>
2025-08-31 10:56:19 +0530

    motifications imp starts

--- nf-server/main.go
+++ nf-server/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"mukulpretham/betterUpPublisher/utils"
-
-	"github.com/redis/go-redis/v9"
 )
 
 func main(){
-	
+	client := utils.CreateRedisClient("localhost:6379",0,"",2)
+	res,err := utils.CreateRedisGroup(client,"notifications","notificationGroup")
+	if err != nil{
+		log.Fatal(err)
+	}
+	fmt.Print(res)
 }
